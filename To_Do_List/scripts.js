54ee905181803a0a5af2a9867af19f822382ea4c
SakethSaimhaReddy <130767932+SakethSaimhaReddy@users.noreply.github.com>
2023-05-25 19:40:21 +0530

    update

--- To_Do_List/scripts.js
+++ To_Do_List/scripts.js
@@ -1,12 +1,12 @@
 let task = document.querySelector('#task');
 let addTask = document.querySelector('#addTask');
-
+let taskArea= document.querySelector('#taskArea')
 addTask.addEventListener('click',function(){
     let list =document.createElement('div');
     list.classList.add('list');
      
     let li =document.createElement('li');
-    li.innerText='$(task.value)';
+    li.innerText=task.value;
     list.appendChild(li);
     let checkButton = document.createElement('button');
     checkButton.innerHTML='<i class="fa fa-check"></i>';
@@ -14,7 +14,29 @@ addTask.addEventListener('click',function(){
     list.appendChild(checkButton)
     
     let deleteButton = document.createElement('button');
-    deleteButton.innerHTML='<i class="fa fa-trash-can"></i>';
+    deleteButton.innerHTML='<i class="fa fa-close"></i>';
     deleteButton.classList.add('deleteButton');
-    list.appendChild(checkButton)
+    list.appendChild(deleteButton)
+
+    if(task.value==="")
+    {
+        alert('please enter the a Task')
+
+    }
+    else{
+        taskArea.appendChild(list)
+    }
+    
+    task.value=""
+    checkButton.addEventListener('click',function(){
+        checkButton.parentElement.style.textDecoration='line-through';
+    })
+    deleteButton.addEventListener('click',function(e){
+        let target =e.target
+        
+        target.parentElement.remove();
+    
+
+
+    })
 })

baa284981b8540ac73ed82e1e8c5cb4be14131e6
SakethSaimhaReddy <130767932+SakethSaimhaReddy@users.noreply.github.com>
2023-05-28 19:57:13 +0530

    q

---
+++ weather/script.js
@@ -0,0 +1,7 @@
+let searchbtn = document.querySelector('#searchbtn');
+let search = document.getElementById("search");
+const apiKey = 'YOUR_API_KEY';
+
+searchbtn.addEventListener('click',()=>{
+    
+})
